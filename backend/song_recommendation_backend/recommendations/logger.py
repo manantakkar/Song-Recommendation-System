0b5ca1b7e496213c7074c4055ab3fd773daa541e
manantakkar18 <manantakkar18@gmail.com>
2024-04-14 15:35:17 +0530

    vite error resolved

--- backend/song_recommendation_backend/recommendations/logger.py
+++ backend/song_recommendation_backend/recommendations/logger.py
@@ -5,12 +5,5 @@ logger.add(
     filter=lambda record: record["extra"].get("name") == "audit",
 )
 
-logger.add(
-    "/app/logs/exception.log",
-    filter=lambda record: record["extra"].get("name") == "exception",
-)
-
-
 
 audit_logger = logger.bind(name="audit")
-exception_logger = logger.bind(name="exception")
